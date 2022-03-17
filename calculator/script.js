1950ba34d686d7685e69c6284bec76bbb6dadb36
IgorMarchenko1 <98159771+IgorMarchenko1@users.noreply.github.com>
2022-03-17 20:04:03 +0300

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,27 @@
+const calc = document.querySelector('.calc');
+const result = document.querySelector('#result');
+
+calc.addEventListener('click', function(event) {
+    if (!event.target.classList.contains('calc__btn')) return;
+
+
+    const valye = event.target.innerText;
+
+
+    switch (valye) {
+        case 'C':
+            result.innerText = '';
+            break;
+
+        case '=':
+            if (result.innerText.search(/[^0-9*/+-.]/mi) != -1) return;
+            result.innerText = eval(result.innerText).toFixed(2);
+            break;
+
+        default:
+            result.innerText += valye;
+
+
+
+    }
+});
